aa5911d78ee0ad844f8d080ae7843e5b60975541
punki03 <tonny_jho@hotmail.com>
2018-06-03 22:15:23 -0500

    Ejercicio4

---
+++ Angular/Ejercicio4/app.js
@@ -0,0 +1,7 @@
+"use strict";
+var nombre = "Jhonatan";
+var apellido = "Saul";
+var edad = 32;
+//let texto = "Hola " + nombre + " " + apellido + "(" +edad  +")";
+var texto = "Hola, " + nombre + " " + apellido + " (" + edad + ")";
+console.log(texto);
---
+++ Angular/Ejercicio4/app.ts
@@ -0,0 +1,6 @@
+let nombre:string = "Jhonatan";
+let apellido:string = "Saul";
+let edad:number = 32;
+//let texto = "Hola " + nombre + " " + apellido + "(" +edad  +")";
+let texto = `Hola, ${nombre} ${apellido} (${edad})`;
+console.log(texto);
