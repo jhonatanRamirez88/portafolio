caee999c4185ae4aa823f55306a8642f263e2531
punki03 <tonny_jho@hotmail.com>
2018-06-03 21:54:41 -0500

    Ejercicio3

---
+++ Angular/Ejercicio3/app.js
@@ -0,0 +1,7 @@
+var nombre = "Jhonatan";
+var numero = 123;
+var boleano = true;
+var hoy = new Date();
+hoy = new Date('2020-10-10');
+console.log(hoy);
+console.log("hoy");
---
+++ Angular/Ejercicio3/app.ts
@@ -0,0 +1,24 @@
+let nombre:string = "Jhonatan";
+
+let numero:number = 123;
+let boleano:boolean = true;
+let hoy:Date = new Date();
+hoy = new Date('2020-10-10');
+console.log(hoy);
+
+let cualquiera:any;
+
+cualquiera = nombre;
+cualquiera = numero;
+cualquiera = boleano;
+cualquiera = hoy;
+
+let spiderman = {
+    nombre:"Joaquin",
+    edad:12
+}
+
+spiderman= {
+    nombre: "Juan",
+    edad:30
+}
