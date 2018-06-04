6a8f259e97848546a47a29ec4507b7d4707ce730
punki03 <tonny_jho@hotmail.com>
2018-06-03 22:26:12 -0500

    Ejercicio5

---
+++ Angular/ES/Ejercicio5/app.js
@@ -0,0 +1,13 @@
+"use strict";
+function activar(quien, objeto, momento) {
+    if (objeto === void 0) { objeto = "batiseñal"; }
+    var mensaje;
+    if (momento) {
+        mensaje = quien + " activo la " + objeto + " en la " + momento;
+    }
+    else {
+        mensaje = quien + " activo la " + objeto;
+    }
+    console.log(mensaje);
+}
+activar("Gordon", "batiseñal", "tarde");
